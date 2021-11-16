93bcf921e08cbd0b1f68a800308f81115a2f8dea
JuliWolf <qwert0038@yandex.ru>
2021-11-16 22:24:38 +0300

    Lesson 7

--- 2. Java Head/1. Base/src/java_head/base/Main.java
+++ 2. Java Head/1. Base/src/java_head/base/Main.java
@@ -438,238 +438,281 @@ import java.util.ArrayList;
 
 
 // ------------------- Версия 2
-class DotComBust {
-  private GameHelper helper = new GameHelper();
-  private ArrayList<DotCom> dotComsList = new ArrayList<DotCom>();
-  private int numOfGuesses = 0;
-
-  private void setUpGame () throws IOException {
-//  Создадим несколько "сайтов" и присвоим им адреса
-    DotCom one = new DotCom();
-    one.setName("Pets.com");
-    DotCom two = new DotCom();
-    two.setName("eToys.com");
-    DotCom three = new DotCom();
-    three.setName("Go2.com");
-
-    dotComsList.add(one);
-    dotComsList.add(two);
-    dotComsList.add(three);
-
-    System.out.println("Ваша цель - потопить три 'сайта'");
-    System.out.println("Pets.com, eToys.com, Go2.com");
-    System.out.println("Попытайтесь потопить их за минимальное количество ходов");
-
-    for (DotCom dotComToSet: dotComsList) {
-      ArrayList<String> newLocation = helper.placeDotCom(3);
-      dotComToSet.setLocationCells(newLocation);
-    }
-  }
-
-  private void startPlaying () throws IOException {
-    while(!dotComsList.isEmpty()) {
-      String userGuess = helper.getUserInput("Сделайте ход");
-      checkUserGuess(userGuess);
-    }
-
-    finishGame();
-  }
-
-  private  void checkUserGuess (String userGuess) {
-    numOfGuesses++;
-
-    String result = "Мимо";
-
-    for(DotCom dotComToTest: dotComsList) {
-      result = dotComToTest.checkYourself(userGuess);
-
-      if(result.equals("Попал")) {
-        break;
-      }
-
-      if (result.equals("Потопил")) {
-        dotComsList.remove(dotComToTest);
-        break;
-      }
-    }
-
-    System.out.println(result);
-  }
-
-  private void finishGame() {
-    System.out.println("Все 'сайты' ушли ко дну! Ваши акции теперь ничего не стоят.");
-
-    if (numOfGuesses <= 18) {
-      System.out.println("Это заняло у вас всего " + numOfGuesses + " попыток.");
-      System.out.println("вы успели выбраться до того, как ваши вложения утонули.");
-    }else {
-      System.out.println("Это заняло у вас довольно много времени. " + numOfGuesses + " попыток.");
-      System.out.println("Рыбы водят хороводы вокруг ваших вложений.");
-    }
-  }
-
-  public static void main (String[] args) throws IOException {
-    DotComBust game = new DotComBust();
-    game.setUpGame();
-    game.startPlaying();
-  }
-}
-
-class GameHelper {
-  private static final String alphabet = "abcdefg";
-  private int gridLength = 7;
-  private int gridSize = 49;
-  private int [] grid = new int[gridSize];
-  private int comCount = 0;
-
-  public String getUserInput(String prompt) throws IOException {
-    String inputLine = null;
-
-    System.out.print(prompt + " ");
-
-    try {
-      BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
-
-      inputLine = is.readLine();
-
-      if (inputLine.length() == 0) return null;
-    } catch (IOException e) {
-      System.out.println("IOException: " + e);
-    }
-
-    return inputLine;
-  }
-
-  public ArrayList<String> placeDotCom(int comSize) {
-    ArrayList<String> alphaCells = new ArrayList<String>();
-    String [] alphacoords = new String [comSize]; // Хранит координаты типы f6
-    String temp = null; //Временная строка для конкатенации
-    int [] coords = new int[comSize]; // Координаты текущего сайта
-    int attempts = 0; // Счетчик текущих попыток
-    boolean success = false; // Нашли подходящее местолопожение?
-    int location = 0; // Текущее начальное местоположение
-
-    comCount++; // энный сайта для размещения
-    int incr = 1; // Устанавливаем горизонтальны инкремент
-
-    if ((comCount % 2) == 1) { // Если нечетный - размещаем вертикально
-      incr = gridLength; // Устанавливаем вертикальны инкремент
-    }
-
-    while (!success & attempts++ < 200) { // Главный поисковый цикл
-      location = (int) (Math.random() * gridSize); // Получаем случайную стратовую строку
-
-      System.out.print("пробуем" + location);
-
-      int x = 0; // Энная позиция в сайте, который нужно разместить
-      success = true; // Предполагаем успешны исход
-      while (success && x < comSize) { // Ищем соседнюю неиспользованную ячейку
-        if (grid[location] == 0) { // Если еще не используется
-          coords[x++] = location; // Сохраняем местоположение
-          location += incr; // Попробуем следующую соседнюю ячейку
-
-          if (location >= gridSize) { // Вышли за рамки - низ
-            success = false; // Неудача
-          }
-
-          if (x > 0 && (location % gridLength == 0)) { // вышли за рамки - правый край
-            success = false; // Неудача
-          }
-        } else { // Нашли уже использующееся положение
-          System.out.print("используется" + location);
-
-          success = false; // Неудача
-        }
-      }
-    }
-
-    int x = 0;
-    int row = 0; // Переводим местоположение в символьные координаты
-    int column = 0;
-
-    System.out.println("\n");
-    while (x < comSize) {
-      grid[coords[x]] = 1; // Помечаем ячейки на главной сетке как использованные
-      row = (int) (coords[x] / gridLength); // Получаем значение строки
-      column = coords[x] % gridLength; // Получаем числовое значение столбца
-      temp = String.valueOf(alphabet.charAt(column)); // Преобразуем его в строковой символ
-
-      alphaCells.add(temp.concat(Integer.toString(row)));
-      x++;
-
-      System.out.print("  coord" + x + " = " + alphaCells.get(x - 1));
-    }
-
-    System.out.println("\n");
-
-    return alphaCells;
-  }
-}
-
-class DotCom {
-  private String name;
-  private ArrayList<String> locationCells;
-
-  public void setLocationCells(ArrayList<String> loc) {
-    locationCells = loc;
-  }
-
-  public void setName(String n) {
-    name = n;
-  }
-
-  public String checkYourself(String userInput) {
-    String result = "Мимо";
-
-//  Проверяем содержится ли загаданная пользователем ячейка внутри ArrayList
-//  Если ее нет в списке, то indexOf вернет -1
-    int index = locationCells.indexOf(userInput);
-
-    if(index >= 0) {
-      locationCells.remove(index);
-
-      if (locationCells.isEmpty()) {
-        result = "Потопил";
-      } else {
-        result = "Попал";
-      }
-    }
-    return result;
-  }
-}
+//class DotComBust {
+//  private GameHelper helper = new GameHelper();
+//  private ArrayList<DotCom> dotComsList = new ArrayList<DotCom>();
+//  private int numOfGuesses = 0;
+//
+//  private void setUpGame () throws IOException {
+////  Создадим несколько "сайтов" и присвоим им адреса
+//    DotCom one = new DotCom();
+//    one.setName("Pets.com");
+//    DotCom two = new DotCom();
+//    two.setName("eToys.com");
+//    DotCom three = new DotCom();
+//    three.setName("Go2.com");
+//
+//    dotComsList.add(one);
+//    dotComsList.add(two);
+//    dotComsList.add(three);
+//
+//    System.out.println("Ваша цель - потопить три 'сайта'");
+//    System.out.println("Pets.com, eToys.com, Go2.com");
+//    System.out.println("Попытайтесь потопить их за минимальное количество ходов");
+//
+//    for (DotCom dotComToSet: dotComsList) {
+//      ArrayList<String> newLocation = helper.placeDotCom(3);
+//      dotComToSet.setLocationCells(newLocation);
+//    }
+//  }
+//
+//  private void startPlaying () throws IOException {
+//    while(!dotComsList.isEmpty()) {
+//      String userGuess = helper.getUserInput("Сделайте ход");
+//      checkUserGuess(userGuess);
+//    }
+//
+//    finishGame();
+//  }
+//
+//  private  void checkUserGuess (String userGuess) {
+//    numOfGuesses++;
+//
+//    String result = "Мимо";
+//
+//    for(DotCom dotComToTest: dotComsList) {
+//      result = dotComToTest.checkYourself(userGuess);
+//
+//      if(result.equals("Попал")) {
+//        break;
+//      }
+//
+//      if (result.equals("Потопил")) {
+//        dotComsList.remove(dotComToTest);
+//        break;
+//      }
+//    }
+//
+//    System.out.println(result);
+//  }
+//
+//  private void finishGame() {
+//    System.out.println("Все 'сайты' ушли ко дну! Ваши акции теперь ничего не стоят.");
+//
+//    if (numOfGuesses <= 18) {
+//      System.out.println("Это заняло у вас всего " + numOfGuesses + " попыток.");
+//      System.out.println("вы успели выбраться до того, как ваши вложения утонули.");
+//    }else {
+//      System.out.println("Это заняло у вас довольно много времени. " + numOfGuesses + " попыток.");
+//      System.out.println("Рыбы водят хороводы вокруг ваших вложений.");
+//    }
+//  }
+//
+//  public static void main (String[] args) throws IOException {
+//    DotComBust game = new DotComBust();
+//    game.setUpGame();
+//    game.startPlaying();
+//  }
+//}
+//
+//class GameHelper {
+//  private static final String alphabet = "abcdefg";
+//  private int gridLength = 7;
+//  private int gridSize = 49;
+//  private int [] grid = new int[gridSize];
+//  private int comCount = 0;
+//
+//  public String getUserInput(String prompt) throws IOException {
+//    String inputLine = null;
+//
+//    System.out.print(prompt + " ");
+//
+//    try {
+//      BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+//
+//      inputLine = is.readLine();
+//
+//      if (inputLine.length() == 0) return null;
+//    } catch (IOException e) {
+//      System.out.println("IOException: " + e);
+//    }
+//
+//    return inputLine;
+//  }
+//
+//  public ArrayList<String> placeDotCom(int comSize) {
+//    ArrayList<String> alphaCells = new ArrayList<String>();
+//    String [] alphacoords = new String [comSize]; // Хранит координаты типы f6
+//    String temp = null; //Временная строка для конкатенации
+//    int [] coords = new int[comSize]; // Координаты текущего сайта
+//    int attempts = 0; // Счетчик текущих попыток
+//    boolean success = false; // Нашли подходящее местолопожение?
+//    int location = 0; // Текущее начальное местоположение
+//
+//    comCount++; // энный сайта для размещения
+//    int incr = 1; // Устанавливаем горизонтальны инкремент
+//
+//    if ((comCount % 2) == 1) { // Если нечетный - размещаем вертикально
+//      incr = gridLength; // Устанавливаем вертикальны инкремент
+//    }
+//
+//    while (!success & attempts++ < 200) { // Главный поисковый цикл
+//      location = (int) (Math.random() * gridSize); // Получаем случайную стратовую строку
+//
+//      System.out.print("пробуем" + location);
+//
+//      int x = 0; // Энная позиция в сайте, который нужно разместить
+//      success = true; // Предполагаем успешны исход
+//      while (success && x < comSize) { // Ищем соседнюю неиспользованную ячейку
+//        if (grid[location] == 0) { // Если еще не используется
+//          coords[x++] = location; // Сохраняем местоположение
+//          location += incr; // Попробуем следующую соседнюю ячейку
+//
+//          if (location >= gridSize) { // Вышли за рамки - низ
+//            success = false; // Неудача
+//          }
+//
+//          if (x > 0 && (location % gridLength == 0)) { // вышли за рамки - правый край
+//            success = false; // Неудача
+//          }
+//        } else { // Нашли уже использующееся положение
+//          System.out.print("используется" + location);
+//
+//          success = false; // Неудача
+//        }
+//      }
+//    }
+//
+//    int x = 0;
+//    int row = 0; // Переводим местоположение в символьные координаты
+//    int column = 0;
+//
+//    System.out.println("\n");
+//    while (x < comSize) {
+//      grid[coords[x]] = 1; // Помечаем ячейки на главной сетке как использованные
+//      row = (int) (coords[x] / gridLength); // Получаем значение строки
+//      column = coords[x] % gridLength; // Получаем числовое значение столбца
+//      temp = String.valueOf(alphabet.charAt(column)); // Преобразуем его в строковой символ
+//
+//      alphaCells.add(temp.concat(Integer.toString(row)));
+//      x++;
+//
+//      System.out.print("  coord" + x + " = " + alphaCells.get(x - 1));
+//    }
+//
+//    System.out.println("\n");
+//
+//    return alphaCells;
+//  }
+//}
+//
+//class DotCom {
+//  private String name;
+//  private ArrayList<String> locationCells;
+//
+//  public void setLocationCells(ArrayList<String> loc) {
+//    locationCells = loc;
+//  }
+//
+//  public void setName(String n) {
+//    name = n;
+//  }
+//
+//  public String checkYourself(String userInput) {
+//    String result = "Мимо";
+//
+////  Проверяем содержится ли загаданная пользователем ячейка внутри ArrayList
+////  Если ее нет в списке, то indexOf вернет -1
+//    int index = locationCells.indexOf(userInput);
+//
+//    if(index >= 0) {
+//      locationCells.remove(index);
+//
+//      if (locationCells.isEmpty()) {
+//        result = "Потопил";
+//      } else {
+//        result = "Попал";
+//      }
+//    }
+//    return result;
+//  }
+//}
 
 
 // ------------------- Test page 191
-class ArrayListMagnet {
+//class ArrayListMagnet {
+//  public static void main (String[] args) {
+//    ArrayList<String> a = new ArrayList<String>();
+//
+//    a.add(0, "ноль");
+//    a.add(1, "один");
+//    a.add(2, "два");
+//    a.add(3, "три");
+//    printAL(a);
+//
+//    if (a.contains("три")) {
+//      a.add("четыре");
+//    }
+//
+//    a.remove(2);
+//
+//    printAL(a);
+//
+//    if (a.indexOf("четыре") != 4) {
+//      a.add(4, "4.2");
+//    }
+//    printAL(a);
+//    printAL(a);
+//  }
+//
+//  public static void printAL(ArrayList<String> al) {
+//    for (String element: al) {
+//      System.out.print(element + " ");
+//    }
+//
+//    System.out.println(" ");
+//  }
+//}
+
+
+// ------------------- Test page 224
+ class TestBoats {
   public static void main (String[] args) {
-    ArrayList<String> a = new ArrayList<String>();
-
-    a.add(0, "ноль");
-    a.add(1, "один");
-    a.add(2, "два");
-    a.add(3, "три");
-    printAL(a);
-
-    if (a.contains("три")) {
-      a.add("четыре");
-    }
-
-    a.remove(2);
-
-    printAL(a);
-
-    if (a.indexOf("четыре") != 4) {
-      a.add(4, "4.2");
-    }
-    printAL(a);
-    printAL(a);
-  }
-
-  public static void printAL(ArrayList<String> al) {
-    for (String element: al) {
-      System.out.print(element + " ");
-    }
-
-    System.out.println(" ");
+    Boat b1 = new Boat();
+    Sailboat b2 = new Sailboat();
+    Rowboat b3 = new Rowboat();
+    b2.setLength(32);
+    b1.move();
+    b3.move();
+    b2.move();
   }
 }
+
+class Boat {
+  private int length;
+
+  private int getLength(){
+    return length;
+  }
+
+  public void setLength (int len) {
+    length = len;
+  }
+
+  public void move(){
+    System.out.print("Дрейф");
+  }
+}
+
+class Sailboat extends Boat {
+   public void rowTheBoat () {
+     System.out.print("Дава, Наташа!");
+   }
+}
+
+class Rowboat extends Boat {
+   public void move() {
+     System.out.print("Поднять паруса!");
+   }
+}
+
